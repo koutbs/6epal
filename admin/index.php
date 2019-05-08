f1d7357da01ab67016225b2165ef7063c3db1a28
koutbs <35882344+koutbs@users.noreply.github.com>
2019-05-08 22:03:59 +0300

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,53 @@
+<?php 
+require_once "../head.php";
+?>
+
+	<title>Διαχείριση</title>
+  </head>
+  <body>
+<?php 
+require_once "menu.php";
+?> 
+
+<div class="container jumbotron top">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+
+<div class="container shadow p-3 mb-5 bg-white rounded">
+	<form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Example select</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlSelect2">Example multiple select</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+</div>
+
+
---
+++ admin/menu.php
@@ -0,0 +1,33 @@
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+  <a class="navbar-brand" href="index.php">Το Blog μου</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../index.php"><i class="fas fa-home"></i> Αρχική <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="../articles.php"><i class="fas fa-newspaper"></i> Άρθρα</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Κατηγορίες
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Κατηγορία 1</a>
+          <a class="dropdown-item" href="#">Κατηγορία 2</a>
+          <a class="dropdown-item" href="#">Κατηγορία 3</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Διαχείριση</a>
+      </li>
+    </ul>
+
+  </div>
+</nav>
